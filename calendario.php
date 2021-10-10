60e32c8149d9ae5c78cff2f1cc5fa25fd4192e3a
Adilson-Lima-ti <82540961+Adilson-Lima-ti@users.noreply.github.com>
2021-10-10 11:11:28 -0300

    iniciando curso PHP

---
+++ calendario.php
@@ -0,0 +1,14 @@
+<h1><?php echo "Título dentro do H1"; ?></h1>
+
+<table border="1">
+	<tr>
+		<th>Dom</th>
+		<th>Seg</th>
+		<th>Ter</th>
+		<th>Qua</th>
+		<th>Qui</th>
+		<th>Sex</th>
+		<th>Sáb</th>
+
+	</tr>
+</table>
---
+++ programa.php
@@ -0,0 +1,22 @@
+﻿
+
+<?php
+echo "hoje é dia ".date('d/m/y')."<br>";
+echo " Agora são ".date('h:i:s');
+
+// Exibe alguma coisa como: Agora são 10:32:11Sunday
+echo date("l")."<br>";
+
+// Exibe alguma coisa como: Monday 8th of August 2005 03:12:46 PM
+echo date('l jS \of F Y h:i:s A')."<br>";
+
+// Exibe: July 1, 2000 is on a Saturday
+echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000))."<br>";
+
+/* utiliza as constantes do parâmetro de formato */
+// Exibe alguma coisa como: Mon, 15 Aug 2005 15:12:46 UTC
+echo date(DATE_RFC822)."<br>";
+
+// Exibe alguma coisa como: 2000-07-01T00:00:00+00:00
+echo date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000))."<br>";
+?>
