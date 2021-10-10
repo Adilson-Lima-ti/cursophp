b02b1f009ab6a7d4ac887b0706ddbcc1051dfd83
Adilson-Lima-ti <82540961+Adilson-Lima-ti@users.noreply.github.com>
2021-10-10 12:03:57 -0300

    final exercício calendário

--- calendario.php
+++ calendario.php
@@ -1,5 +1,40 @@
-<h1><?php echo "Título dentro do H1"; ?></h1>
+ <?php 
+   function linha($semana)
+   	{
+		echo "<tr>";
+		for ($i = 0; $i <=6; $i++) {
+			if (isset($semana[$i])) { //isset() verifica se uma variável foi definida.
+				echo "<td>{$semana[$i]}</td>";
+			} else {
+				
+				echo"<td></td>";
+			}
+		}
+		echo "</tr>";
+		
+	} 
 
+	function calendario()
+	{
+		$dia = 1;
+		$semana = array();
+		while ($dia <= 31) {
+			array_push($semana, $dia);
+
+			if (count($semana)==7) {
+				linha($semana);
+				$semana = array();
+			}
+
+			$dia++;
+
+
+		}
+		linha($semana);
+	}
+
+
+?>
 <table border="1">
 	<tr>
 		<th>Dom</th>
@@ -11,4 +46,6 @@
 		<th>Sáb</th>
 
 	</tr>
+	<?php calendario(); ?>
+	
 </table>
